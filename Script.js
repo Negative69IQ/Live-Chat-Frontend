19b3b5db4ab7ba9f5483c55e52dc48e8cbbbe186
Negative69IQ <142377199+Negative69IQ@users.noreply.github.com>
2023-08-16 02:32:51 -0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,21 @@
+const chatLog = document.getElementById('chat-log');
+const messageInput = document.getElementById('message-input');
+const sendButton = document.getElementById('send-button');
+
+const socket = new WebSocket('wss://your-websocket-server-url');
+
+socket.addEventListener('message', (event) => {
+    const message = event.data;
+    const messageElement = document.createElement('div');
+    messageElement.textContent = message;
+    chatLog.appendChild(messageElement);
+});
+
+sendButton.addEventListener('click', () => {
+    const message = messageInput.value;
+    if (message) {
+        socket.send(message);
+        messageInput.value = '';
+    }
+});
 
