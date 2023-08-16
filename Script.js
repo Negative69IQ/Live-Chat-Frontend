289ed36a8065c02ec2f0cb5b60e7551999355841
Negative69IQ <142377199+Negative69IQ@users.noreply.github.com>
2023-08-16 03:07:57 -0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -2,7 +2,7 @@ const chatLog = document.getElementById('chat-log');
 const messageInput = document.getElementById('message-input');
 const sendButton = document.getElementById('send-button');
 
-const socket = new WebSocket('wss://your-websocket-server-url');
+const socket = new WebSocket('https://freckle-lowly-chrysanthemum.glitch.me');
 
 socket.addEventListener('message', (event) => {
     const message = event.data;
